e62abca31935fbab4ef1c68c9315a81b22a5c7bf
aman1210 <aman.srivastava101@gmail.com>
2024-02-03 22:10:09 +0530

    make it better

--- app.js
+++ app.js
@@ -27,22 +27,7 @@ mongoose
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin,X-Requested-With,Content-Type,Accept,Authorization"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET,POST,PATCH,PUT,DELETE,OPTIONS"
-  );
-  next();
-});
-
 app.use(cors());
-
-app.options("*", cors());
 
 app.use("/api/users", usersRoute);
 app.use("/api/songs", songsRoute);
