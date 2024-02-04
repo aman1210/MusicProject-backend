dfdc85cc7c9a32baac8ba0c72489cd11d09a3820
aman1210 <aman.srivastava101@gmail.com>
2024-02-04 07:16:23 +0530

    removing dotenv

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const cors = require("cors");
-require("dotenv").config();
 
 const usersRoute = require("./routes/usersRoute");
 const artistsRoute = require("./routes/artistsRoute");
